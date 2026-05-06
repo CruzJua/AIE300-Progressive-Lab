99ad9b69ad7e54c54533a7c7929af92688acb0ea
Juan Cruz <tonocruz9000@gmail.com>
2026-05-06 15:54:28 -0600

    Feat: Finished lab 3

--- frontend/js/root.js
+++ frontend/js/root.js
@@ -1,11 +1,38 @@
-const API_URL = "http://localhost:8000/items";
+const API_URL = "http://localhost:8000/";
 
 const itemContainer = document.getElementById("item-container");
 const itemForm = document.getElementById("item-form");
 const itemName = document.getElementById("item-name");
 const itemDescription = document.getElementById("item-description");
 
+const modelForm = document.getElementById("model-form");
+const sepalLength = document.getElementById("sepal-length");
+const sepalWidth = document.getElementById("sepal-width");
+const petalLength = document.getElementById("petal-length");
+const petalWidth = document.getElementById("petal-width");
 
+
+modelForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const model = {
+        features: [sepalLength.value, sepalWidth.value, petalLength.value, petalWidth.value],
+    };
+
+    try {
+        const res = await fetch(API_URL + 'predict', {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(model)
+        });
+        if (!res.ok) throw new Error("Failed to pretict Iris");
+        const created = await res.json();
+        addIrisCard(created);
+        modelForm.reset();
+    } catch (err) {
+        console.error(err);
+        alert("Error creating item");
+    }
+});
 itemForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     const item = {
@@ -14,7 +41,7 @@ itemForm.addEventListener("submit", async (e) => {
     };
 
     try {
-        const res = await fetch(API_URL, {
+        const res = await fetch(API_URL + 'items', {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(item)
@@ -37,7 +64,7 @@ document.addEventListener("DOMContentLoaded", () => {
 async function renderItems() {
     itemContainer.innerHTML = "Loading...";
     try {
-        const res = await fetch(API_URL);
+        const res = await fetch(API_URL + 'items');
         if (!res.ok) throw new Error("Failed to fetch items");
         const items = await res.json();
 
@@ -73,9 +100,28 @@ function addItemCard(item) {
     itemContainer.appendChild(itemCard);
 }
 
+function addIrisCard(item) {
+    if (itemContainer.textContent === "NO ITEMS FOUND") {
+        itemContainer.innerHTML = "";
+    }
+    const itemCard = document.createElement("div");
+    itemCard.classList.add("item-card");
+    itemCard.dataset.id = item.id;
+    item.description = `This is an ${item.prediction} that was predicted with ${item.confidence*100}% confidence.`; 
+    item.name = item.prediction;
+    itemCard.innerHTML = `
+        <p>Item Name: ${item.name}</p>
+        <p>Item Description: ${item.description}</p>
+        <div class="item-card-buttons">
+            <button onclick="editItem('${item.id}')">Edit</button>
+            <button onclick="deleteItem('${item.id}')">Delete</button>
+        </div>
+    `;
+    itemContainer.appendChild(itemCard);
+}
 async function deleteItem(id) {
     try {
-        const res = await fetch(`${API_URL}/${id}`, { method: "DELETE" });
+        const res = await fetch(`${API_URL}items/${id}`, { method: "DELETE" });
         if (!res.ok) throw new Error("Failed to delete item");
         renderItems();
     } catch (err) {
@@ -91,7 +137,7 @@ async function editItem(id) {
     if (newDesc === null) return;
 
     try {
-        const res = await fetch(`${API_URL}/${id}`, {
+        const res = await fetch(`${API_URL}items/${id}`, {
             method: "PUT",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ name: newName, description: newDesc, id: id })
